6c8de4d49634857f1d92c6b801e93d6ee3bd10f0
chenyirou <annie851216@gmail.com>
2018-05-15 10:09:44 +0800

    button_Click

--- app/src/main/java/com/example/admin/ch8_2/MainActivity.java
+++ app/src/main/java/com/example/admin/ch8_2/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.admin.ch8_2;
 
+import android.content.DialogInterface;
+import android.preference.DialogPreference;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void button_Click(View v){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("關於")
+                .setMessage("版本:7.0版 \n 作者:陳會安")
+                .setPositiveButton(
+                        "確定"
+                        , new DialogInterface.OnClickListener(){
+                            public void onClick(DialogInterface dialogInterface, int i){
+
+                            }
+                        })
+                .show();
+    }
+
 }
