299cbf5e3aa40cb519a2e06011aeeed0f0aeb69d
chenyirou <annie851216@gmail.com>
2018-05-15 11:09:29 +0800

    確認對話方塊 動態綁定onClick

--- app/src/main/java/com/example/admin/ch8_2/MainActivity.java
+++ app/src/main/java/com/example/admin/ch8_2/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +18,9 @@ public class MainActivity extends AppCompatActivity {
 
         Button btn_message = (Button)findViewById(R.id.btn_message);
         btn_message.setOnClickListener(btn_message_listener);
+
+        Button btn_confirm = (Button)findViewById(R.id.btn_confirm);
+        btn_confirm.setOnClickListener(btn_confirm_Click);
     }
 
     DialogInterface.OnClickListener dialog_listener = new DialogInterface.OnClickListener(){
@@ -34,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
                     .show();
         }
     };
+    /*非動態*/
     /*public void button_Click(View v){
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("關於")
@@ -47,5 +52,28 @@ public class MainActivity extends AppCompatActivity {
                         })
                 .show();
     }*/
+
+    DialogInterface.OnClickListener btn_confirm_P_Click = new DialogInterface.OnClickListener(){
+        public void onClick(DialogInterface dialogInterface, int i){
+            finish();
+        }
+    };
+
+    DialogInterface.OnClickListener btn_confirm_N_Click = new DialogInterface.OnClickListener(){
+        public void onClick(DialogInterface dialogInterface, int i){
+            Toast.makeText(MainActivity.this, "按下取消按鈕", Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    View.OnClickListener btn_confirm_Click = new View.OnClickListener(){
+        public void onClick(View v){
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setTitle("確認")
+                    .setMessage("確認結束本程式")
+                    .setPositiveButton("確定", btn_confirm_P_Click)
+                    .setNegativeButton("取消", btn_confirm_N_Click)
+                    .show();
+        }
+    };
 
 }
